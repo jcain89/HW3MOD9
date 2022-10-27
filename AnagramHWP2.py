2894d58b76f54d5b8195a1235e3d33a45984a1d1
jcain89 <cainj4@mail.uc.edu>
2022-10-27 15:50:21 -0400

    got rid of unneccessary prints

--- AnagramHWP2.py
+++ AnagramHWP2.py
@@ -46,8 +46,6 @@ def step(s):
                 sol.append(a)
         wordSet = list(s)
     print(sol)
-    print(type(wordSet))
-
 #I changed my time complexity from O(n!) to O(n)
 
 
@@ -60,5 +58,5 @@ def step(s):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(step("HERAT"))
+    step("HERAT")
 
